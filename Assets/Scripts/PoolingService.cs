b879f5a61f615d48a3cbc045c427fb59a3526e86
slayer-tej <aruntejagurrapu@gmail.com>
2021-05-04 10:01:19 +0530

    added basic scoreholder with prefab loading from assetbundle

---
+++ Assets/Scripts/PoolingService.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+public class PoolingService<T> : MonoSingletonGeneric<PoolingService<T>> where T : class
+{
+    private List<poolingItem<T>> pooledItems = new List<poolingItem<T>>();
+
+    public virtual T GetItem()
+    {
+        if (pooledItems.Count > 0)
+        {
+            poolingItem<T> pooledItem = pooledItems.Find(i => !i.isActive);
+            if (pooledItem != null)
+            {
+                pooledItem.isActive = true;
+                return pooledItem.item;
+            }
+        }
+        return CreateNewPooledItem();
+    }
+
+    public virtual void ReturnItem(T item)
+    {
+        poolingItem<T> poolingItem = pooledItems.Find(i => i.item.Equals(item));
+        poolingItem.isActive = false;
+    }
+
+    private T CreateNewPooledItem()
+    {
+        poolingItem<T> poolItem = new poolingItem<T>();
+        poolItem.item = CreateItem();
+        poolItem.isActive = true;
+        pooledItems.Add(poolItem);
+        //print(pooledItems.Count + "Instance Created" );
+        return poolItem.item;
+    }
+
+    protected virtual T CreateItem()
+    {
+        return (T)null;
+    }
+
+    public class poolingItem<U>
+    {
+        public U item;
+        public bool isActive;
+    }
+}
---
+++ Assets/Scripts/ScoreBoardController.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.IO;
+
+[Serializable]
+public struct ScoreBoardEntryData
+{
+    public string entryName;
+    public int entryScore;
+}
+
+[Serializable]
+public class ScoreBoardSaveData
+{
+    public List<ScoreBoardEntryData> highScores = new List<ScoreBoardEntryData>();
+}
+
+
+public class ScoreBoardController : MonoBehaviour
+{
+    [SerializeField]
+    private int maxScoreBoardEntries = 5;
+    [SerializeField]
+    private Button button;
+    [SerializeField]
+    private Transform highScoresHolder;
+    private UnityEngine.Object m_scoreBoardEntryObject;
+    private LoadAssetBundles m_loadAssetBundle;
+    private string SavePath;
+    private ScoreBoardEntryData data;
+    
+
+    private void Start()
+    {
+        ScoreBoardSaveData savedScores = GetSavedScores();
+        m_loadAssetBundle = gameObject.GetComponent<LoadAssetBundles>();
+        SavePath = Path.Combine(Application.persistentDataPath, "HighScores.Json");
+        data.entryName = "N/A";
+        data.entryScore = 0;
+        if (savedScores == null)
+        {
+            savedScores = new ScoreBoardSaveData();
+        }
+        UpdateUI(savedScores);
+        SaveScores(savedScores);
+        button.onClick.AddListener(OnClick);
+    }
+
+    private void OnClick()
+    {
+        highScoresHolder.gameObject.SetActive(true);
+    }
+
+    public void AddEntry(ScoreBoardEntryData scoreBoardEntryData)
+    {
+        ScoreBoardSaveData savedScores = GetSavedScores();
+        bool scoreAdded = false;
+        for (int i = 0; i < savedScores.highScores.Count; i++)
+        {
+            if (scoreBoardEntryData.entryScore > savedScores.highScores[i].entryScore)
+            {
+                savedScores.highScores.Insert(i, scoreBoardEntryData);
+                scoreAdded = true;
+                break;
+            }
+        }
+        if (!scoreAdded && savedScores.highScores.Count < maxScoreBoardEntries)
+        {
+            savedScores.highScores.Add(scoreBoardEntryData);
+        }
+        if (savedScores.highScores.Count > maxScoreBoardEntries)
+        {
+            savedScores.highScores.RemoveRange(maxScoreBoardEntries, savedScores.highScores.Count - maxScoreBoardEntries);
+        }
+        UpdateUI(savedScores);
+        SaveScores(savedScores);
+    }
+
+    private void UpdateUI(ScoreBoardSaveData savedScores)
+    {
+        foreach (Transform child in highScoresHolder)
+        {
+            Destroy(child.gameObject);
+        }
+        foreach (ScoreBoardEntryData highscore in savedScores.highScores)
+        {
+            m_scoreBoardEntryObject = m_loadAssetBundle.GetBundleObject();
+
+            GameObject scoreCard = Instantiate(m_scoreBoardEntryObject, highScoresHolder) as GameObject;
+            scoreCard.GetComponent<SetScore>().SetBoard(highscore);
+        }
+    }
+
+    private ScoreBoardSaveData GetSavedScores()
+    {
+        if (!File.Exists(SavePath))
+        {
+            File.Create(SavePath).Dispose();
+            ScoreBoardSaveData newSavedData = new ScoreBoardSaveData();
+            return newSavedData;
+        }
+        using (StreamReader stream = new StreamReader(SavePath))
+        {
+            string json = stream.ReadToEnd();
+            return JsonUtility.FromJson<ScoreBoardSaveData>(json);
+        }
+    }
+
+    private void SaveScores(ScoreBoardSaveData scoreBoardSaveData)
+    {
+        using (StreamWriter stream = new StreamWriter(SavePath))
+        {
+            string json = JsonUtility.ToJson(scoreBoardSaveData, true);
+            stream.Write(json);
+        }
+    }
+}
